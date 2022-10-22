ee1ec605e3d6e5849abfb331c04e5841dcf2aa47
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2022-10-22 22:39:54 +0530

    SQL Functionality

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-  .then(result => {
-    console.log(result[0], result[1]);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,4 +22,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(4000);
